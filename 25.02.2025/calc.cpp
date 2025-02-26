49b3f897969b51e5101334002ef4f5935b391f97
ArtzAsrian <asryan091267532@gmail.com>
2025-02-27 02:04:23 +0400

    Create calc.cpp

---
+++ 25.02.2025/calc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void calc(){
+        int num1 = 0;
+        std::cout<<"num1";
+        std::cin>>num1;
+        int num2 = 0;
+        std::cout<<"num2";
+        std::cin>>num2;
+        int num3 = num1 + num2;
+        std::cout<<num3;
+
+}
+int main(){
+        calc();
+}
